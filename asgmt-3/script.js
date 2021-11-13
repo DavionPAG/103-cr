44a3febb1c5fdd3ff434b942be6040088fef831e
Davion <daviongarcia@gmail.com>
2021-11-13 10:36:43 -0800

    103 asgmt 3

---
+++ asgmt-3/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let results = document.getElementById('results')
+
+document.getElementById("calc").addEventListener("click", calc);
+
+
+function calc() {
+  let num1 = prompt('Enter a number');
+  let num2 = prompt('Enter anonther number');
+
+  results.textContent =
+    `<ul>
+      <li>${num1} + ${num2} = ${parseInt(num1) + parseInt(num2)}</li>
+      <li>${num1} - ${num2} = ${num1 - num2};</li>
+      <li>${num1} / ${num2} = ${num1 / num2}</li>
+      <li>${num1} * ${num2} = ${num1 * num2}</li>
+    </ul>`
+}
+
+
